5252ca4238e418bb66ed1da56d11436b15b09290
edwinpeligro85 <edwinflow888@gmail.com>
2019-11-23 00:36:01 -0500

    Añadiendo las rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/Home', pathMatch: 'full'},
+  {path: 'Home', component: HomeComponent},
+  {path: 'About', component: AboutComponent},
+  {path: 'Heroes', component: HeroesComponent},
+  {path: '**', redirectTo: '/Home', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
